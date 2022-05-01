1100c5f2feb348cd372d804bb6de28759c200f6f
Sodmunkh <99303047+Sodmunkh@users.noreply.github.com>
2022-05-01 23:51:04 +0800

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import numpy as np
+import pandas as pd
+pd.__version__
+data0 = pd.read_excel("Бие даалт - 1.xlsx", sheet_name = "Ирц")
+data0
+
+
+# In[ ]:
+
+
+
+
